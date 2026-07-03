8d47473a314082304959316c8eaa6a90637d7788
nilam <nilam62@gmail.com>
2026-07-03 13:56:48 +0530

    Fix build script - add createsuperuser line

--- build.sh
+++ build.sh
@@ -6,3 +6,13 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 #!/usr/bin/env bash
 set -o errexit
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+# Create the admin login automatically from environment variables.
+# Safe to run every deploy - does nothing if the user already exists.
+python manage.py createsuperuser --noinput || true
